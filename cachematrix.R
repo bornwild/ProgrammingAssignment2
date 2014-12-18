03ad3bbd8723ae9f02dd6e130951d5d9df2d7ee8
bornwild <vinod.k.kumar@hotmail.com>
2014-12-18 11:39:19 +0530

    Added methods and sample

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,45 @@
 ## Creates a custom inverse cacheable  matrix .Input is a matrix 
 ## Inverse calcualtion assumes that the matrix which is invertible 
 
-makeCacheMatrix <- function(x = matrix()) {
+## Sample 
+# > k <- matrix(c(4,7,2,6),2,2)
+# > x <- makeCacheMatrix(k)
+# > cacheSolve(x)
+#     [,1] [,2]
+# [1,]  0.6 -0.2
+# [2,] -0.7  0.4
+# > cacheSolve(x)
+# getting cached inverse
+#     [,1] [,2]
+# [1,]  0.6 -0.2
+# [2,] -0.7  0.4
 
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(argInv) inv <<- argInv
+  getInv <- function() inv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
 ## Computes the inverse of the matrix lazily 
+## Input to function is a matrix created by the makeCacheMatrix function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  inv <- x$getInv()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  mx <- x$get()
+  inv <- solve(mx) ## Built in R function solve
+  x$setInv(inv)
+  inv
 }
